bc9f3ec3820eb06f86ad831b2729281f1863ecf7
Ido <games@ido.wtf>
2020-01-17 14:12:16 +0200

    simplified version works

--- UnitySDK/Assets/ML-Agents/Examples/_MyGame/Scripts/SilenceAgentSimplified.cs
+++ UnitySDK/Assets/ML-Agents/Examples/_MyGame/Scripts/SilenceAgentSimplified.cs
@@ -6,8 +6,8 @@ using Random = UnityEngine.Random;
 
 public class SilenceAgentSimplified : Agent
 {
-    public const int CollectionSize = StoreSize + BagSize;
-    private static readonly int[] Empty = new int[ItemsTypes]{0,0};
+    public const int CollectionSize = ItemsTypes * (StoreSize + BagSize);
+    private static readonly int Empty = -1;
     private const int ItemsTypes = 2;
     private const int StoreSize = 1;
     private const int BagSize = 2;
@@ -22,13 +22,10 @@ public class SilenceAgentSimplified : Agent
 
     public SilenceAcademySimplified academy;
     
-    [HideInInspector]
-    public int[][] store = new int[StoreSize][] {Empty};
-    [HideInInspector]
-    public int[][] bag = new int[BagSize][] {Empty,Empty};
+    private int store = Empty;
+    private int[] bag = new int[BagSize] {Empty,Empty};
     private int bagItems = 0;
-    private int itemsOrdered = 0;
-    private int badItemsNum = 0;
+    private int actionsNum = 0;
 
     public override void InitializeAgent()
     {
@@ -38,125 +35,77 @@ public class SilenceAgentSimplified : Agent
     public override void CollectObservations()
     {
         AddVectorObs(store);
-        AddVectorObs(bag);
+        AddVectorObs(bag[0]);
+        AddVectorObs(bag[1]);
     }
 
-    private void AddVectorObs(int[][] items)
-    {
-        foreach (var item in items)
-        foreach (var val in item)
-            AddVectorObs(val);
-    }
-
-    public void Win()
-    {
-        Done();
-        SetReward(1);
-    }
-
-    public void Lose()
-    {
-        Done();
-        SetReward(-1);
-    }
-    
+    private ACTIONS action;
+    private int index;
     public override void AgentAction(float[] vectorAction)
     {
-        if (badItemsNum > 0)
-            AddReward(-0.1f);
-        
-        var action = (ACTIONS) Mathf.RoundToInt(vectorAction[0]);
-        var index = Mathf.RoundToInt(vectorAction[1]);
-        switch (action)
+        actionsNum++;
+ 
+        action = (ACTIONS) Mathf.RoundToInt(vectorAction[0]);
+        if (action == ACTIONS.RETURN)
         {
-            case ACTIONS.BUY:
-                bag[bagItems] = store[index];
-                store[index] = OrderItem();
-                AddItem(bag[bagItems]);
-                bagItems++;
-                if (bagItems == BagSize)
-                {
-                    FinishGame();
-                    return;
-                }
-                break;
-            case ACTIONS.RETURN:
-                if (bagItems == 0)
-                {
-                    AddReward(-0.1f);
-                    return;
-                }
+            if (bagItems == 0)
+            {
+                SetReward(-0.1f);
+                return;
+            }
 
-                RemoveItem(bag[index]);
-                for (var i = index; i < bagItems; i++)
-                    bag[i] = bag[i + 1];
-                bag[bagItems] = Empty;
-                bagItems--;
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
+            bag[0] = Empty;
+            bagItems--;
         }
-
-        if (itemsOrdered > ItemsOrderLimit)
-            FinishGame();
+        else if (action == ACTIONS.BUY)
+        {
+            bag[bagItems] = store;
+            store = Random.Range(0, 2);
+            bagItems++;
+        }
         
+        if (bagItems == BagSize)
+        {
+            var win = bag[0] == bag[1];
+            PrintState(win);
+            if (win)
+            {
+                Done();
+                SetReward(1);
+            }
+            else
+            {
+                Done();
+                SetReward(-1);
+            }
+        }
+        else
+        {
+            SetReward(0);
+        }
     }
 
     public override void AgentReset()
     {
-        badItemsNum = 0;
+        actionsNum = 0;
         bagItems = 0;
-        itemsOrdered = 0;
-
-        for (var i = 0; i < store.Length; i++)
-            store[i] = OrderItem();
-        for (var i = 0; i < bag.Length; i++)
-            bag[i] = Empty;
-    }
-
-    private int[] OrderItem()
-    {
-        itemsOrdered++;
-        var vec = new int[ItemsTypes]{0,0};
-        vec[Random.Range(0, vec.Length)]++; 
-        return vec;
-    }
-
-    private void FinishGame()
-    {
-        PrintState();
-        if (badItemsNum == 0)
-            Win();
-        else
-            Lose();
+        store = Random.Range(0, 2);
+        bag[0] = Empty;
+        bag[1] = Empty;
     }
 
     public override float[] Heuristic()
     {
-        return new float[] { Random.Range(0,(int)ACTIONS.COUNT), 0 };
+        return new float[] { Random.Range(0,(int)ACTIONS.COUNT) };
     }
 
     public override void AgentOnDone()
     {
     }
 
-    private void AddItem(int[] item)
+    private void PrintState(bool win)
     {
-        if (item[0] == 1)
-            badItemsNum++;
-    }
-
-    private void RemoveItem(int[] item)
-    {
-        if (item[0] == 1)
-            badItemsNum--;
-    }
-
-    private void PrintState()
-    {
-        var shopState = string.Join(" ", store.Select(card => string.Join(",", card.Select(i => i.ToString()).ToArray()) ).ToArray());
-        var bagState = string.Join(" ", bag.Select(card => string.Join(",", card.Select(i => i.ToString()).ToArray()) ).ToArray());
-        
-        Debug.Log( "count:" + badItemsNum + " shop[" + itemsOrdered +"]:" + shopState + ", bag[" + bagItems + "]:" + bagState);
+        if(!win)
+            Debug.Log( "count:" + actionsNum + " shop:" + store + ", bag:" + bag[0] + "," + bag[1]);
     }
 }
